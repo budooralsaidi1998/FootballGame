da1bdb99f1631114e837a370834b88449c5855b4
budoor <budooralsidi1998@gmail.com>
2024-10-09 14:02:52 +0400

    add class player and it's attribute construct

---
+++ Players.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballGameAssignment
+{
+    public class Players
+    {
+        public int number_player {  get; private set; }
+        public enum Position
+        {
+            Forward, Midfielder, Defender, Goalkeeper
+        }
+        public Position position { get; private set; }
+
+        public string NamePlayer { get; private set; }
+
+
+        public Players( int numplayer ,Position pos , string nameplayer )
+        {
+            number_player = numplayer;
+            position = pos;
+            NamePlayer = nameplayer;
+
+        }
+
+    }
+}
