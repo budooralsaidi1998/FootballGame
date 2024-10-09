3fae789b212639ed341c2c32488a7cce67bca0c7
budoor <budooralsidi1998@gmail.com>
2024-10-09 11:53:12 +0400

    connect repo footballGame

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FootballGameAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
