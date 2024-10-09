ac5664e551988a8cd0e0b535a69059fc23dbee30
budoor <budooralsidi1998@gmail.com>
2024-10-09 15:41:44 +0400

    add AutomaticallyGeneratePlayer method and generatenameplayer method

--- Teams.cs
+++ Teams.cs
@@ -6,22 +6,50 @@ using System.Threading.Tasks;
 
 namespace FootballGameAssignment
 {
-    public  class Teams
+    public class Teams
     {
 
         public string NameTeams { get; private set; }
+        public List<Players> players { get; private set; } = new List<Players>();
 
-        public Teams( string nameteam) 
+        Random random = new Random();
+        public Teams(string nameteam)
         {
 
             NameTeams = nameteam;
         }
 
+        public void AutomaticallyGeneratPlayer()
+        {
+
+            string[] position = { "Forward", "Midfielder", "Defender", "Goalkeeper" };
+           
+            for (int i = 1; i < 11; i++)
+            {
+                string playername = GenerateNamePlayer();
+
+                string positionrandom = position[random.Next(position.Length)];
+
+                int skilllevel=random.Next(1, 100);
+
+                players.Add(new Players(i, playername, positionrandom, skilllevel));
+
+            }
 
 
 
 
 
 
+        }
+
+        public string GenerateNamePlayer()
+        {
+            string[] namepalyer = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K" };
+
+            string name = namepalyer[random.Next(namepalyer.Length)];
+
+            return name;
+        }
     }
 }
