9e68d3599c2501b163981725f2ee314fbff77794
budoor <budooralsidi1998@gmail.com>
2024-10-09 13:42:13 +0400

    add class teams and add attribute name team with contruste

---
+++ Teams.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballGameAssignment
+{
+    public  class Teams
+    {
+
+        public string NameTeams { get; private set; }
+
+        public Teams( string nameteam) 
+        {
+
+            NameTeams = nameteam;
+        }
+
+
+
+
+
+
+
+    }
+}
