133ba28eb77c838b9334d7de8f7e91ba5c7dc99b
budoor <budooralsidi1998@gmail.com>
2024-10-10 13:47:49 +0400

    add interface to implement method Fgame

---
+++ IGameActions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballGameAssignment
+{
+    public  interface IGameActions
+    {
+        void StartGame();
+        void SimulateTurnTeams(Teams attackingTeam, Teams defendingTeam);
+        void DisplayResults();
+        void PlayHalf(string half);
+    }
+}
