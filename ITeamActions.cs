f810c9157dd6c90570f0a839c6977f93c33403b9
budoor <budooralsidi1998@gmail.com>
2024-10-10 13:40:43 +0400

    add interface to impement the team by the method

---
+++ ITeamActions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballGameAssignment
+{
+    public  interface ITeamActions
+    {
+        void AutomaticallyGeneratPlayer();
+        int AttackPlayer();
+        int defencePlayer();
+        void DisplayPayer();
+    }
+}
